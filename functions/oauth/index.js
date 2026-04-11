262a8c9247a8c0d74616dd3ed3202d8895fc7acc
Cameron Gaut <seegodesign@gmail.com>
2026-04-10 17:19:22 -1000

    fix: add check for missing GITHUB_CLIENT_ID environment variable

--- functions/oauth/index.js
+++ functions/oauth/index.js
@@ -1,4 +1,8 @@
 export async function onRequest({ request, env }) {
+  if (!env.GITHUB_CLIENT_ID) {
+    return new Response('Missing GITHUB_CLIENT_ID environment variable', { status: 500 });
+  }
+
   const params = new URLSearchParams({
     client_id: env.GITHUB_CLIENT_ID,
     scope: 'repo,user',
